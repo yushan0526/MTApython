1b7d5fb5e3a1d621aff7ab43880c65419ec92581
yushan0526 <78331189+yushan0526@users.noreply.github.com>
2021-02-01 10:47:10 +0800

    Add files via upload

---
+++ D1.1test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 09:28:28 2021
+
+@author: MTAEXAM
+"""
+
+height=float(input("長:"))
+wide=float(input("寬:"))
+area=height*wide
+print("長方形面積:"+str(area))
+
---
+++ D1.2test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 10:02:26 2021
+
+@author: MTAEXAM梯形面積
+"""
+top=float(input("top:"))
+bottom=float(input("bottom:"))
+height=float(input("height:"))
+area=(top+bottom)*height/2
+print("梯形面積:"+str(area))
+
+
+
